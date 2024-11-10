e64f575d1561b76da6ce8cf0047016e9488b9002
crftwr <craftware@gmail.com>
2024-11-09 16:30:14 -0800

    Moved "Edit config.py" from ConsoleWindow to Menu

--- Keyhac/ApplicationLayer/ConsoleWindowView.swift
+++ Keyhac/ApplicationLayer/ConsoleWindowView.swift
@@ -48,43 +48,6 @@ struct ConsoleWindowView: View {
             SwiftTermView( viewController: termViewController )
                 .lookupKey(termViewKey)
                 .frame(minWidth: 100, minHeight: 50, alignment: .center)
-
-            HStack {
-                Button("Edit config.py"){
-
-                    let config_file_path = NSString(string: "~/.keyhac/config.py").expandingTildeInPath
-                    let editors = ["Visual Studio Code", "Xcode", "TextEdit"]
-                    var resultCode:Int32 = 0
-                    var errorMessage: String = ""
-                    
-                    for editor in editors {
-                        let p = Process()
-                        let pipe = Pipe()
-                        p.launchPath = "/usr/bin/open"
-                        p.arguments = ["-a", editor, config_file_path ]
-                        p.standardError = pipe
-                        p.launch()
-                        p.waitUntilExit()
-                        resultCode = p.terminationStatus
-                        if resultCode == 0 {
-                            break
-                        }
-                        else {
-                            let errorMessageData = pipe.fileHandleForReading.readDataToEndOfFile()
-                            errorMessage = String(data: errorMessageData, encoding: .utf8)!
-                        }
-                    }
-                    
-                    if resultCode == 0 {
-                        Console.getInstance().write(s: "Opened config.py\n")
-                    }
-                    else {
-                        Console.getInstance().write(s: "Error: ")
-                        Console.getInstance().write(s: errorMessage)
-                    }
-                }
-            }
-            .padding(.all, 2)
         }
         .padding(.all, 10)
     }
